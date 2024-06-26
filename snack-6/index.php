e87a259dee5e44577917e3749d90b9246f80bd38
SimoneColombo1 <simcolombo2@gmail.com>
2024-06-27 01:48:33 +0200

    snack 6

---
+++ snack-6/index.php
@@ -0,0 +1,75 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    foreach ($posts as $date => $postsArray) {
+    ?> <h2> <?php echo 'Data:' . $date ?></h2>
+        <?php foreach ($postsArray as $post) {
+
+
+        ?>
+            <div style=' padding: 1rem; margin-bottom: 2rem; background-color: grey;'>
+                <h3> <?php echo $post['title']; ?> </h3>
+                <p>Autore: <?php echo $post['author']; ?> </p>
+                <p> <?php echo $post['text']; ?> </p>
+            </div>
+
+    <?php
+        }
+    }
+    ?>
+</body>
+
+</html>
